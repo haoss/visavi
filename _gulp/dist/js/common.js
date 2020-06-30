fe243e61d9b1eb8964e18443ab4b1d26bbea1b68
Dmitriy Khristoev <haossut@gmail.com>
2020-06-30 12:46:59 +0300

    new styles

--- _gulp/dist/js/common.js
+++ _gulp/dist/js/common.js
@@ -10,21 +10,6 @@ $(document).on('ready', function(){
       type: 'image',
       gallery:{
         enabled:true
-      },
-      zoom: {
-        enabled: true, // By default it's false, so don't forget to enable it
-
-        duration: 300, // duration of the effect, in milliseconds
-        easing: 'ease-in-out', // CSS transition easing function
-
-        // The "opener" function should return the element from which popup will be zoomed in
-        // and to which popup will be scaled down
-        // By defailt it looks for an image tag:
-        opener: function(openerElement) {
-          // openerElement is the element on which popup was initialized, in this case its <a> tag
-          // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-          return openerElement.is('img') ? openerElement : openerElement.find('img');
-        }
       }
     });
   });
@@ -53,6 +38,7 @@ $(document).on('ready', function(){
     type: 'inline',
     midClick: true,
     showCloseBtn: false,
+    mainClass: 'mfp-bg-gallery',
     callbacks: {
       beforeOpen: function() {
         $('.j-btn-mobile').removeClass('is-active');
